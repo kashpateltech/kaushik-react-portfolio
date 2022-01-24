bb29d75b8d6a06ab828c2ae1defe709a501fa6e5
Kash Patel <kaushikpatel26@yahoo.com>
2022-01-24 20:06:35 +1100

    upadted work.js className

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -4,7 +4,7 @@ function Work() {
     const projects = [
         {
             name: 'ARTHOUSE',
-            image: '../assets/images/arthouse.jpeg',
+            image: '/assets/images/arthouse.jpeg',
             deployed: 'https://raimeeab.github.io/arthouse-project/',
             github: 'https://github.com/Raimeeab/arthouse-project/tree/home_page',
             desc: 'ArtHouse is real world application designed for users to search for various artists and their artwork.Technologies used in this project are HTML, CSS, Foundation, JavaScript and Third party APIs',
@@ -45,7 +45,7 @@ function Work() {
                                                         alt="project-image" />
                                                 </div>
                                             </div>
-                                            <div className="post-title"><a href={project.github} target="_blank">{project.name} </a><span class="mai-logo-github"></span></div>
+                                            <div className="post-title"><a href={project.github} target="_blank">{project.name} </a><span className="mai-logo-github"></span></div>
                                             <div className="entry-content"><a href={project.deployed} target="_blank">Deployed Link</a></div>
                                             <div className="entry-content">
                                                 <p>{project.desc}</p>
